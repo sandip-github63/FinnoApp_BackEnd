99f86740d2075b0377f29e083a9ee1efed330648
sandip <mrsandipkumar63@gmail.com>
2023-12-15 21:00:04 +0530

    Basic Setup Done

---
+++ src/main/java/com/finnoapp/FinnoAppApplication.java
@@ -0,0 +1,13 @@
+package com.finnoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finnoapp/FinnoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finnoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
